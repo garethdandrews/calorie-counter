4a41f6590a23a2b21958c02abc8537c6d2a5d2c5
gda <garethdandrews@gmail.com>
2021-02-05 23:12:41 +0000

    added role name to role

--- backend-api/Domian/Models/Role.cs
+++ backend-api/Domian/Models/Role.cs
@@ -6,6 +6,7 @@ namespace backend_api.Domain.Models
     public class Role
     {
         public int Id { get; set; }
+        public string Name { get; set; }
         public ICollection<UserRole> UsersRole { get; set; } = new Collection<UserRole>();
     }
 }
