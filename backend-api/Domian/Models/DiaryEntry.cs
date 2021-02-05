1e932e04d3248be7c8277e4a8b8ff2cf425a2599
gda <garethdandrews@gmail.com>
2021-02-05 21:41:57 +0000

    added calorie target to diary entries table

--- backend-api/Domian/Models/DiaryEntry.cs
+++ backend-api/Domian/Models/DiaryEntry.cs
@@ -8,6 +8,7 @@ namespace backend_api.Domain.Models
         public int Id { get; set; }
         public DateTime Date { get; set; }
         public int TotalCalories { get; set; } = 0;
+        public int CalorieTarget { get; set; }
         public IList<FoodItem> FoodItems { get; set; } = new List<FoodItem>();
 
         public User User { get; set; }
---
+++ backend-api/Migrations/20210205213959_addCalorieTargetToDiaryEntry.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace backend_api.Migrations
+{
+    public partial class addCalorieTargetToDiaryEntry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CalorieTarget",
+                table: "DiaryEntries",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CalorieTarget",
+                table: "DiaryEntries");
+        }
+    }
+}
