28b2d07dd9f7c0cbfadf670b06b3116cd88e595a
gda <garethdandrews@gmail.com>
2021-02-07 19:11:13 +0000

    cleaned solution

---
+++ frontend/src/App/App.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Router, Route, Link } from 'react-router-dom';
+
+import { history } from '../_helpers';
+
+class App extends React.Component {
+  constructor(props) {
+      super(props);
+
+      this.state = {
+          currentUser: null
+      };
+  }
+
+  componentDidMount() {
+      
+  }
+
+  render() {
+      const { currentUser } = this.state;
+      return (
+          <Router history={history}>
+              <div>
+                  {currentUser &&
+                      <nav className="navbar navbar-expand navbar-dark bg-dark">
+                          <div className="navbar-nav">
+                              <Link to="/" className="nav-item nav-link">Home</Link>
+                          </div>
+                      </nav>
+                  }
+                  <div className="jumbotron">
+                      <div className="container">
+                          <div className="row">
+                              <div className="col-md-6 offset-md-3">
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </Router>
+      );
+  }
+}
+
+export { App }; 
