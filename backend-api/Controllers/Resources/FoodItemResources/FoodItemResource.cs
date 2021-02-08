e5fe0d2b5a31b31aaf8f13044bf6a2ae3edbe63f
gda <garethdandrews@gmail.com>
2021-02-08 19:25:01 +0000

    return the ids of food items

--- backend-api/Controllers/Resources/FoodItemResources/FoodItemResource.cs
+++ backend-api/Controllers/Resources/FoodItemResources/FoodItemResource.cs
@@ -2,6 +2,7 @@ namespace backend_api.Controllers.Resources.FoodItemResources
 {
     public class FoodItemResource
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Calories { get; set; }
     }
