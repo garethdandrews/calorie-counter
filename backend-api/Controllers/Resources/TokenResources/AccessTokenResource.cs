21faadcc96420f71bfbc6c5c6da18379bd281160
gda <garethdandrews@gmail.com>
2021-02-08 12:02:24 +0000

    return the username in the access token

--- backend-api/Controllers/Resources/TokenResources/AccessTokenResource.cs
+++ backend-api/Controllers/Resources/TokenResources/AccessTokenResource.cs
@@ -2,6 +2,7 @@ namespace backend_api.Controllers.Resources.TokenResources
 {
     public class AccessTokenResource
     {
+        public string Name { get; set; }
         public string AccessToken { get; set; }
         public string RefreshToken { get; set; }
         public long Expiration { get; set; }
